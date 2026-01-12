d56952a3131963d54754ca8da3ea7e9e560c0299
Scorpion <122550188+CSistRayan@users.noreply.github.com>
2026-01-12 02:05:29 -0500

    im getting nowhere with this blud

---
+++ re/player/scripts/player_MainScript.gd
@@ -0,0 +1,32 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+
+func _physics_process(delta: float) -> void:
+	# gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	var input_dir := Input.get_vector("in_left", "in_right", "in_forward", "in_backward")
+	
+	# Apply rotation
+	if input_dir.x == 1:
+		rotate_y(0.02)
+	elif input_dir.x == -1:
+		rotate_y(-0.02)
+	
+	# Apply movement
+	if input_dir.y == 0:
+		velocity = self.global_transform.basis.z * 0
+	elif input_dir.y < 0:
+		velocity = self.global_transform.basis.z * 1
+	elif input_dir.y > 0:
+		velocity = self.global_transform.basis.z * -1
+	
+	print(input_dir.y)
+
+
+	move_and_slide()
