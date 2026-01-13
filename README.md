934795eda8cf91bad3bf1b86d7280383235b5b14
Scorpion <122550188+CSistRayan@users.noreply.github.com>
2026-01-13 01:52:34 -0500

    Real work starts here!
    I hate movement! but its done now!

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 2025t_project
+
+### Open Source Credits/Materials:
+```
+- Tank Controls Base: 
+https://godotforums.org/d/36094-3d-tank-controls-for-player-movement/5
+```
+
--- re/player/scripts/player_MainScript.gd
+++ re/player/scripts/player_MainScript.gd
@@ -1,26 +1,42 @@
+
 extends CharacterBody3D
+ 
+@export var movement_Speed = 2.0
+@export var turning_Speed = 0.025
 
 
-const SPEED = 3.0
-const JUMP_VELOCITY = 4.5
-const turn_speed = 5
 
-
-func _physics_process(delta: float) -> void:
-	# gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _physics_process(_delta: float) -> void:
+	#
+	### MOVEMENT SCRIPT
+	### original (https://godotforums.org/d/36094-3d-tank-controls-for-player-movement/5)
+	### modified to fix rotation issues
+	#
+	var input_direction : Vector2 =  Input.get_vector("in_right","in_left","in_backward", "in_forward")
+	var movement_direction = input_direction.y
 	
-	var movement_input = Input.get_axis("in_forward", "in_backward")
-	var direction_input = Input.get_axis("in_right", "in_left")
-	var controller_direction_input = Input.get_axis("in2_right", "in2_left")
+	# prevent backward walking
+	if input_direction.y < 0.0:
+		# Fix later
+		movement_direction = abs(movement_direction * movement_Speed)
+	else:
+		movement_direction = movement_direction * movement_Speed
 	
-	# rotation
-	rotate_y(direction_input * turn_speed * delta)
-	rotate_y(controller_direction_input * turn_speed * delta)
 	
-	var velocity_vector = -global_transform.basis.z * movement_input * SPEED
+	velocity = self.global_transform.basis.z * movement_direction
 	
-	velocity.x = velocity_vector.x
-	velocity.z = velocity_vector.z
+	if movement_direction == 0:
+	# Apply rotation WHILE static
+		if input_direction.y > 0.0:
+			rotate_y(turning_Speed * -input_direction.x)
+		else:
+			rotate_y(turning_Speed * input_direction.x)
+	else:
+	# Apply rotation WHILE moving
+		if input_direction.y > 0.0:
+			rotate_y(turning_Speed * input_direction.x)
+		else:
+			rotate_y(turning_Speed * -input_direction.x)
+	### End of Movement
+	
 	move_and_slide()
